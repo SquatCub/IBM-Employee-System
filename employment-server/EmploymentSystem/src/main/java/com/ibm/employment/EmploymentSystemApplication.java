3618e7290f169ef0928ea3588806e5be9f232474
Brandon Rodriguez <brandon.alexis10@hotmail.com>
2021-10-28 13:09:54 -0500

    Proyecto terminado

---
+++ employment-server/EmploymentSystem/src/main/java/com/ibm/employment/EmploymentSystemApplication.java
@@ -0,0 +1,30 @@
+package com.ibm.employment;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.data.mongodb.repository.config.EnableMongoRepositories;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@SpringBootApplication
+@EnableMongoRepositories("com.ibm.repository")
+@ComponentScan("com.ibm.*")
+public class EmploymentSystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EmploymentSystemApplication.class, args);
+	}
+	
+	@Bean
+	public WebMvcConfigurer corsConfigurer() {
+        return new WebMvcConfigurer() {
+            @Override
+            public void addCorsMappings(CorsRegistry registry) {
+                registry.addMapping("/**").allowedOrigins("*").allowedMethods("GET", "POST","PUT", "DELETE");
+            }
+        };
+    }
+
+}
